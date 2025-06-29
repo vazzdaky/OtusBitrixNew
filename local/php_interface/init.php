a4de20ed643f402a88c51b4c1fc22e24599061f4
Vladimir Polyakov <vazzdaky@gmail.com>
2025-06-29 15:41:43 +0300

    init.php

--- local/php_interface/init.php
+++ local/php_interface/init.php
@@ -1,4 +1,4 @@
 <?php
-//if (file_exsists(__DIR__ . '/../../vendor/autoload.php')) {
-//    require_once __DIR__ . '/../../vendor/autoload.php';
-//}
+if (file_exists(__DIR__ . '/../../vendor/autoload.php')) {
+    require_once __DIR__ . '/../../vendor/autoload.php';
+}
