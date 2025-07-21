0c334fc0348cd4778290fe7e8644f1a4749af1b9
Vladimir Polyakov <vazzdaky@gmail.com>
2025-07-21 20:45:45 +0300

    Домашнее задание 3

---
+++ local/app/Models/Lists/DoctorsPropertyValuesTable.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Models\Lists;
+
+use Models\AbstractIblockPropertyValuesTable;
+
+class DoctorsPropertyValuesTable extends AbstractIblockPropertyValuesTable
+{
+    const IBLOCK_ID = 20;
+}
--- local/php_interface/init.php
+++ local/php_interface/init.php
@@ -1,4 +1,14 @@
 <?php
-if (file_exists(__DIR__ . '/../../vendor/autoload.php')) {
-    require_once __DIR__ . '/../../vendor/autoload.php';
+// автолоадер проекта Otus
+include_once __DIR__ . '/../app/autoload.php';
+
+// вывод данных 
+function pr($var, $type = false)
+{
+    echo '<pre style="font-size:10px; border:1px solid #000; background:#FFF; text-align:left; color:#000;">';
+    if ($type)
+        var_dump($var);
+    else
+        print_r($var);
+    echo '</pre>';
 }
